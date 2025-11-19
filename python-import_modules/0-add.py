65740886648f49a0741cc1c7dad3828982745762
holbiejlln <11078@holbertonstudents.com>
2025-11-19 12:18:47 +0000

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a+b 
-result=add(1,2)
-print(result)
+"""0-add.py: import add function and print 1 + 2"""
+
+from add_0 import add  # yalnız bir dəfə add_0 istifadə olunur
+
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
