4780c8fc4099ba79ea95eee32fc80ec4c1e8c904
holbiejlln <11078@holbertonstudents.com>
2025-11-21 05:59:01 +0000

    t3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
-if __name__=="__main__":
-    total=0
+if __name__ == "__main__":
+    total = 0
     for arg in sys.argv[1:]:
-        total+=int(arg)
-
+        total += int(arg)
     print(total)
