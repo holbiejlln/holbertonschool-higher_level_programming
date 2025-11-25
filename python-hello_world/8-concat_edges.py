1e969bef233bb525c780c7b275bd2b47fc001897
holbiejlln <11078@holbertonstudents.com>
2025-11-25 17:59:39 +0000

    t6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# Print "object-oriented programming with Python" without string literals
-print("".join([str.__name__[0:6], "-", str.__name__[0:7].lower(), " programming with Python"]))
-
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
